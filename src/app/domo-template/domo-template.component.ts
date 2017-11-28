6da424810451c04ca6f6121eecc7148bff77a645
draffault <denis.raffault.prestataire@prima-solutions.comPrima2017>
2017-11-28 17:25:46 +0100

    - added title component - added sony reactions

--- src/app/domo-template/domo-template.component.ts
+++ src/app/domo-template/domo-template.component.ts
@@ -19,8 +19,8 @@ export class DomoTemplateComponent implements OnInit {
   selectedTab;
   
   mqttFormMessage = { "topic": "home/domo/sonoff01/cmd", "payload": "version" };
-  formTopics = ["home/domo/nodedomo/cmd", "home/domo/espIR01/cmd", "home/domo/espIR02/cmd", "home/domo/sonoff01/cmd", "home/domo/sonoff02/cmd"];
-  formPayloads = ["version", "toggle", "irsend marantz C0D", "irsend nec FF000033"];
+  formTopics = ["home/domo/nodedomo/cmd", "home/domo/espIR01/cmd", "home/domo/espIR02/cmd", "home/domo/sonoff01/cmd", "home/domo/sonoff02/cmd", "home/domo/espIR01/event"];
+  formPayloads = ["version", "toggle", "irsend marantz C0D", "irsend nec FF000033", "rxIR proto=4 code=811 (12 bits)"];
   
   constructor(
     private modalService: NgbModal, 
