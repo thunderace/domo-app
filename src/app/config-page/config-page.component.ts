01e789231af573acc81babe02f3b532685f09755
draffault <denis.raffault.prestataire@prima-solutions.comPrima2017>
2017-12-11 19:00:17 +0100

    - nav bar tests

--- src/app/config-page/config-page.component.ts
+++ src/app/config-page/config-page.component.ts
@@ -9,6 +9,7 @@ import { MqttService } from '../mqtt.service';
 })
 export class ConfigPageComponent implements OnInit {
 
+  selectedTab;
   mqttFormMessage = { "topic": "home/domo/sonoff01/cmd", "payload": "version" };
   formTopics = ["home/domo/nodedomo/cmd", "home/domo/espIR01/cmd", "home/domo/espIR02/cmd", "home/domo/sonoff01/cmd", "home/domo/sonoff02/cmd", "home/domo/espIR01/event"];
   formPayloads = ["version", "toggle", "irsend marantz C0D", "irsend nec FF000033", "rxIR proto=4 code=811 (12 bits)"];
